7102ba5b81938daec97e018f8e0b3689b43c122f
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-22 22:06:49 +0330

    - Create ImageList Property In IIntelligenceListView

--- intelligenceLite/IIntelligenceListView.cs
+++ intelligenceLite/IIntelligenceListView.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace intelligenceLite
 {
     internal interface IIntelligenceListView
     {
+        /// <summary>
+        /// Image list
+        /// </summary>
+        ImageList ImageList { get; set; }
     }
 }
