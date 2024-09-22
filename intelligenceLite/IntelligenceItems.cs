20707394ad69348d6cd6bef335c534f26ded18a5
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-23 00:28:21 +0330

    - Create Compare Override Method In IntelligenceItems Class

--- intelligenceLite/IntelligenceItems.cs
+++ intelligenceLite/IntelligenceItems.cs
@@ -16,5 +16,22 @@ namespace intelligenceLite
         {
             lowercaseText = Text.ToLower();
         }
+
+        public override CompareResult Compare(string fragmentText)
+        {
+            int i = fragmentText.LastIndexOf('.');
+            if (i < 0)
+                return CompareResult.Hidden;
+            string lastPart = fragmentText.Substring(i + 1);
+            firstPart = fragmentText.Substring(0, i);
+
+            if (lastPart == "") return CompareResult.Visible;
+            if (Text.StartsWith(lastPart, StringComparison.InvariantCultureIgnoreCase))
+                return CompareResult.VisibleAndSelected;
+            if (lowercaseText.Contains(lastPart.ToLower()))
+                return CompareResult.Visible;
+
+            return CompareResult.Hidden;
+        }
     }
 }
