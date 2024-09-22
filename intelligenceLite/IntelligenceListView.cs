3f0398d9591fd8ef137b48999d08949d88cb7d56
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-23 00:59:09 +0330

    - Create IntelligenceListView Fields

--- intelligenceLite/IntelligenceListView.cs
+++ intelligenceLite/IntelligenceListView.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Controls;
 
 namespace intelligenceLite
 {
-    public class IntelligenceListView
+    [System.ComponentModel.ToolboxItem(false)]
+    public class IntelligenceListView : UserControl, IIntelligenceListView
     {
+        private readonly System.Windows.Controls.ToolTip toolTip = new System.Windows.Controls.ToolTip();
+        public int HighlightedItemIndex { get; set; }
+        private int oldItemCount;
+        private int selectedItemIndex = -1;
+        private IList<IntelligenceItem> visibleItems;
+
 
     }
 }
