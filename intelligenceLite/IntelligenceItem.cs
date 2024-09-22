9abd9edec64371b6c3da270ce4c132ba38a3412a
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-23 00:23:40 +0330

    - Create CompareResult Enum In intelligenceLite Namespace

--- intelligenceLite/IntelligenceItem.cs
+++ intelligenceLite/IntelligenceItem.cs
@@ -133,4 +133,20 @@ namespace intelligenceLite
 
 
     }
+
+    public enum CompareResult
+    {
+        /// <summary>
+        /// Item do not appears
+        /// </summary>
+        Hidden,
+        /// <summary>
+        /// Item appears
+        /// </summary>
+        Visible,
+        /// <summary>
+        /// Item appears and will selected
+        /// </summary>
+        VisibleAndSelected
+    }
 }
