de77fffbdafdaae764d39c260ddf87b36492b94b
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-23 14:30:58 +0330

    - Create HighlightingColor Property In IntelligenceMenu Component

--- intelligenceLite/IntelligenceMenu.cs
+++ intelligenceLite/IntelligenceMenu.cs
@@ -112,6 +112,17 @@ namespace intelligenceLite
             }
         }
 
+        [Category("Appearance")]
+        public Color HighlightingColor
+        {
+            get { return this.highlightingColor; }
+            set
+            {
+                this.highlightingColor = value;
+                this.ListView.HighlightingColor = value;
+            }
+        }
+
 
         public IntelligenceMenu()
         {
