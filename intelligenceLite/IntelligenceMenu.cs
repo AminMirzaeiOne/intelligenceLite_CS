6458de4b6f7691678ea3bc9ef32d3ec07948deb6
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-23 14:23:46 +0330

    - Create sourceItems Filed And VisibleItems Property In IntelligenceMenu Component

--- intelligenceLite/IntelligenceMenu.cs
+++ intelligenceLite/IntelligenceMenu.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,11 @@ namespace intelligenceLite
 
         private ITextBoxWrapper targetControlWrapper;
         private readonly Timer timer = new Timer();
+
+        private IEnumerable<IntelligenceItem> sourceItems = new List<IntelligenceItem>();
+        [Browsable(false)]
+        public IList<IntelligenceItem> VisibleItems { get { return Host.ListView.VisibleItems; } private set { Host.ListView.VisibleItems = value; } }
+        private Size maximumSize;
 
 
         public IntelligenceMenu()
