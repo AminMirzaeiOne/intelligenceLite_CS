c3e7c1810c7cddf3d227278a0049d3046f3c8abb
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-24 03:32:39 +0330

    - Create Base Fileds And Property In IntelligenceMenuHost Class

--- intelligenceLite/IntelligenceMenuHost.cs
+++ intelligenceLite/IntelligenceMenuHost.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace intelligenceLite
 {
-    public class IntelligenceMenuHost
+    [System.ComponentModel.ToolboxItem(false)]
+    public class IntelligenceMenuHost : ToolStripDropDown
     {
+        private IIntelligenceListView listView;
+        public ToolStripControlHost Host { get; set; }
+        public readonly IntelligenceMenu Menu;
+
 
     }
 }
