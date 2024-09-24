377bc9beeb0ca302117a982405f0000a884b43a9
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-24 03:40:10 +0330

    - Create GetPositionFromCharIndex Function In ITextBoxWrapper

--- intelligenceLite/ITextBoxWrapper.cs
+++ intelligenceLite/ITextBoxWrapper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,5 +16,8 @@ namespace intelligenceLite
         string SelectedText { get; set; }
         int SelectionLength { get; set; }
         int SelectionStart { get; set; }
+
+        Point GetPositionFromCharIndex(int pos);
+
     }
 }
