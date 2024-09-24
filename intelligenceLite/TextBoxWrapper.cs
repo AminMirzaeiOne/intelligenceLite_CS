2b704f44391f969fb2a1547b0bf7b2b7bdfdeb2b
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-24 03:45:41 +0330

    - Create Base Fileds In TextBoxWrapper Class

--- intelligenceLite/TextBoxWrapper.cs
+++ intelligenceLite/TextBoxWrapper.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace intelligenceLite
 {
-    public class TextBoxWrapper
+    public class TextBoxWrapper : ITextBoxWrapper
     {
-
+        private Control target;
+        private PropertyInfo selectionStart;
+        private PropertyInfo selectionLength;
+        private PropertyInfo selectedText;
+        private PropertyInfo readonlyProperty;
+        private MethodInfo getPositionFromCharIndex;
+        private event ScrollEventHandler RTBScroll;
     }
 }
